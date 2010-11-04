c58caa795356f4840253983127f0a85f08bd396b
quiksillvr <quiksillvr@a09ce2f9-38f1-0d4c-c202-f4d1de138eeb>
2010-11-04 21:12:21 +0000

    Formatted code.

--- gr1-manet-simulator/src/logger/MyLogger.java
+++ gr1-manet-simulator/src/logger/MyLogger.java
@@ -20,22 +20,25 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.SimpleLayout;
 
 /**
- * The MyLogger class is used to provide detailed logging about the state of each node, the overall
- * network environment, information about messages (i.e. size) being transferred at each
- * increment of execution.
+ * The MyLogger class is used to provide detailed logging about the state of
+ * each node, the overall network environment, information about messages (i.e.
+ * size) being transferred at each increment of execution.
  * 
- * As well as provide text file logging capability so users can retrieve the results
- * of their execution.
+ * As well as provide text file logging capability so users can retrieve the
+ * results of their execution.
  * 
  * @author MSRoberts1
- *
+ * 
  */
 public class MyLogger {
-     public static Logger logger = Logger.getLogger(Logger.class);
-    static {
-    ConsoleAppender appender = new ConsoleAppender(new SimpleLayout());
-    logger.addAppender(appender);
-    logger.setLevel(Level.INFO);}
-    //PropertyConfigurator.configure("plainlog4jconfig.properties");
-    //DOMConfigurator.configure("xmllog4jconfig.xml")
+	public static Logger logger = Logger.getLogger(Logger.class);
+
+	static {
+		ConsoleAppender appender = new ConsoleAppender(new SimpleLayout());
+		logger.addAppender(appender);
+		logger.setLevel(Level.INFO);
+	}
+	
+	// PropertyConfigurator.configure("plainlog4jconfig.properties");
+	// DOMConfigurator.configure("xmllog4jconfig.xml")
 }
