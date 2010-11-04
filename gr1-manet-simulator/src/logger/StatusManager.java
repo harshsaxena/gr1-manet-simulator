a20f42d133abe254e0e3933331ef8d7f637cffea
quiksillvr <quiksillvr@a09ce2f9-38f1-0d4c-c202-f4d1de138eeb>
2010-11-04 21:12:35 +0000

    Added loggers

--- gr1-manet-simulator/src/logger/StatusManager.java
+++ gr1-manet-simulator/src/logger/StatusManager.java
@@ -14,50 +14,73 @@
 
 package logger;
 
-import simulator.Node;
 import simulator.Data;
+import simulator.Node;
 import UI.Myform;
 import UI.myobjects.GraphicalNode;
 
 public class StatusManager {
-    Myform myform;
-    boolean test = false;
-    private static StatusManager status_manager;
 
-    private StatusManager(Myform myform) {
-        this.myform = myform;
-    }
+	Myform myform;
+	boolean test = false;
+	private static StatusManager status_manager;
 
-    public StatusManager(boolean test) {
-        this.test = test;
-    }
+	private StatusManager(Myform myform) {
+		this.myform = myform;
+	}
 
-    public static void init (Myform myForm){
-         status_manager = new StatusManager(myForm);
-    }
-    public static void init (){
-         status_manager = new StatusManager(true);
-    }
-    public static StatusManager get_instance(){
-        return status_manager;
-    }
+	public StatusManager(boolean test) {
+		this.test = test;
+	}
 
-    public void showNodeStatus(Node node,String status){
-        if (!this.test){
-            GraphicalNode gNode = myform.getGnodebyNode(node);
-            gNode.addStatus(status);
-        }
-    }
-    public void showRecievedData(Node receiver,Node sender, Data data){
-        if (!this.test){
-            GraphicalNode gNode = myform.getGnodebyNode(receiver);
-            gNode.addRecievedData("From: "+sender+"\n"+data.getContent());
-        }
-    }
-    public void NodeSend(Node sender,int type){
-         if (!this.test){
-            GraphicalNode gNode = myform.getGnodebyNode(sender);
-            gNode.sending(type);
-        }
-    }
+	public static void init(Myform myForm) {
+		status_manager = new StatusManager(myForm);
+	}
+
+	public static void init() {
+		status_manager = new StatusManager(true);
+	}
+
+	public static StatusManager get_instance() {
+		return status_manager;
+	}
+
+	/**
+	 * Creates a Graphical Node and add the passed in Status to the Graphical
+	 * Node.
+	 * 
+	 * @param node
+	 * @param status
+	 */
+	public void showNodeStatus(Node node, String status) {
+		if (!this.test) {
+			GraphicalNode gNode = myform.getGnodebyNode(node);
+			gNode.addStatus(status);
+			MyLogger.logger.info("Node " + gNode.getName() + " is " + gNode.getStatus());
+		}
+	}
+
+	/**
+	 * Creates a Graphical Node and adds sender and received data to the
+	 * Graphical Node.
+	 * 
+	 * @param receiver
+	 * @param sender
+	 * @param data
+	 */
+	public void showRecievedData(Node receiver, Node sender, Data data) {
+		if (!this.test) {
+			GraphicalNode gNode = myform.getGnodebyNode(receiver);
+			gNode.addRecievedData("From: " + sender + "\n" + data.getContent());
+			MyLogger.logger.info("Node " + gNode.getName() + " received "
+					+ data.getContent() + " from " + sender);
+		}
+	}
+
+	public void NodeSend(Node sender, int type) {
+		if (!this.test) {
+			GraphicalNode gNode = myform.getGnodebyNode(sender);
+			gNode.sending(type);
+		}
+	}
 }
