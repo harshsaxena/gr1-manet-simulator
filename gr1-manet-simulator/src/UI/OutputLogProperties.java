73d7fcaf1fc45ebc09e82b6bd28132b34f4796c8
quiksillvr <quiksillvr@a09ce2f9-38f1-0d4c-c202-f4d1de138eeb>
2010-11-09 21:57:23 +0000

    Updated properties.

--- gr1-manet-simulator/src/UI/OutputLogProperties.java
+++ gr1-manet-simulator/src/UI/OutputLogProperties.java
@@ -17,7 +17,6 @@ package UI;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -27,80 +26,67 @@ import javax.swing.JTextArea;
 @SuppressWarnings("serial")
 public class OutputLogProperties extends JPanel implements ActionListener {
 
-	public JTextArea dataText;
-	public JTextArea receivedDataText;
-	public JTextArea statusText;
+	public JLabel receivedDataLbl;  // move edit
+	public JLabel statusLbl;  // move edit
 	
-	private Myform myForm;
+	public JTextArea receivedDataText;  // move edit
+	public JTextArea statusText;  // move edit
 	
-	public OutputLogProperties(Myform myForm)
+	//private Myform myForm;
+	
+	public OutputLogProperties(){}
+	
+	public JPanel getOutputLogProperties()
 	{
-		this.setMyForm(myForm);
-		JLabel receivedDataLabel;
-		JLabel statusLabel;
+		JPanel testPanel = new JPanel();
 		
-		//create main layout
-		Box outputLogBox = Box.createHorizontalBox();
-		myForm.add(outputLogBox);
+		// Main Box
+		Box mainOutputLogBox = Box.createHorizontalBox();
+		testPanel.add(mainOutputLogBox);
 		
-		//create third box labels
-		receivedDataLabel = new JLabel("Received Data: ");
-		statusLabel = new JLabel("Status:");
+        // Action Listener
+		// TODO: Create action listener to clear text
+        // ActionListener okAction = new NodePropOKBtnAction(this.myForm);
 		
-		//create third box text fields
-		receivedDataText = new JTextArea(5,20);
-		receivedDataText.setEditable(false);
-		JScrollPane receivedDataScrollPane= new JScrollPane(receivedDataText);
-		
+        /* To Be Moved */
+        receivedDataLbl = new JLabel("RData: ");
+        receivedDataText = new JTextArea(5,20);
+        receivedDataText.setEditable(false);
+        JScrollPane rdataSP= new JScrollPane(receivedDataText);
+        
+        statusLbl = new JLabel("Status: ");
         statusText = new JTextArea(5,20);
         statusText.setEditable(false);
-        JScrollPane statusScrollOane = new JScrollPane(statusText);
+        JScrollPane statusSP = new JScrollPane(statusText);
         
-        //create third box layouts
-        JPanel thirdBox = new JPanel();
-		myForm.add(thirdBox);
-        thirdBox.setBorder(
-                BorderFactory.createCompoundBorder(
-                        BorderFactory.createTitledBorder("Output Log"),
-                        BorderFactory.createEmptyBorder(5,5,5,5)));
+        mainOutputLogBox.add(Box.createHorizontalStrut(10));
         
-        Box v31 = Box.createVerticalBox();
-        thirdBox.add(v31);
+        Box receivedDataLabelAndTextBox = Box.createHorizontalBox();
+        Box receivedLabelGlueBox = Box.createVerticalBox();
+        receivedLabelGlueBox.add(receivedDataLbl);
+        receivedLabelGlueBox.add(Box.createVerticalGlue());
+        receivedDataLabelAndTextBox.add(receivedLabelGlueBox);
+        receivedDataLabelAndTextBox.add(rdataSP);
+        mainOutputLogBox.add(receivedDataLabelAndTextBox);
         
-        Box v311 = Box.createVerticalBox();
-        v311.add(receivedDataLabel);
-        v311.add(Box.createVerticalGlue());
+        mainOutputLogBox.add(Box.createHorizontalStrut(10));
         
-        Box h31 = Box.createHorizontalBox();
-        h31.add(v311);
-        h31.add(receivedDataScrollPane);
+        Box statusDataLabelAndTextBox = Box.createHorizontalBox();
+        Box statusLabelGlueBox = Box.createVerticalBox();
+        statusLabelGlueBox.add(statusLbl);
+        statusLabelGlueBox.add(Box.createVerticalGlue());
+        statusDataLabelAndTextBox.add(statusLabelGlueBox);
+        statusDataLabelAndTextBox.add(statusSP);
+        mainOutputLogBox.add(statusDataLabelAndTextBox);
         
-        v31.add(h31);
-        v31.add(Box.createVerticalStrut(3));
+        mainOutputLogBox.add(Box.createHorizontalStrut(10));
         
-        Box v321 = Box.createVerticalBox();
-        v321.add(statusLabel);
-        v321.add(Box.createVerticalGlue());
+        return testPanel;
         
-        Box h32 = Box.createHorizontalBox();
-        h32.add(v321);
-        h32.add(statusScrollOane);
-        
-        v31.add(h32);
 	}
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void setMyForm(Myform myForm) {
-		this.myForm = myForm;
-	}
-
-	public Myform getMyForm() {
-		return myForm;
-	}
+	
+	public void actionPerformed(ActionEvent e) {
+		// TODO: Add action for clearing the output logs
+    }
 	
 }
