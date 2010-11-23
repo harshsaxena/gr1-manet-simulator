24e047aa6d4939e6fb3a4ae08c27a5742fc54223
quiksillvr <quiksillvr@a09ce2f9-38f1-0d4c-c202-f4d1de138eeb>
2010-11-23 02:48:24 +0000

    trying to fix reset/clear props

--- trunk/gr1-manet-simulator/src/UI/actions/NodePropResetBtnAction.java
+++ trunk/gr1-manet-simulator/src/UI/actions/NodePropResetBtnAction.java
@@ -29,7 +29,7 @@ public class NodePropResetBtnAction implements ActionListener {
         if (myForm.getSelectedGNode()!=null){
             myForm.getSelectedGNode().fillNodePanel();
         }else{
-            myForm.getNodePanel().resetNodePropertiest();
+            myForm.getNodePanel().resetNodeProperties();
         }
     }
 }
