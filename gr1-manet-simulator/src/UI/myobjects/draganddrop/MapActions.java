b329fb263cf9009161e4f7621799235853b8b0c5
quiksillvr <quiksillvr@a09ce2f9-38f1-0d4c-c202-f4d1de138eeb>
2010-12-04 20:32:22 +0000

    Refactored name again.

---
+++ gr1-manet-simulator/src/UI/myobjects/draganddrop/MapActions.java
@@ -0,0 +1,93 @@
+/**  
+ * ----------------------------------------------------------
+ * This software is for educational purposes only.
+ * The base of this software was created by IntelliJ IDEA.
+ * Additions to the base have been made by the Hood College
+ * Computer Science Department, Graduate Group 1.
+ * ----------------------------------------------------------
+ *
+ * History:
+ * @version: $Revision$
+ * @date: $Date$
+ * @author: $Author$
+ */
+
+package UI.myobjects.draganddrop;
+
+import java.awt.Dimension;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetDropEvent;
+import java.io.IOException;
+
+import javax.swing.JComponent;
+import javax.swing.JLayeredPane;
+
+import logger.FileLogger;
+import UI.myobjects.GraphicalNode;
+
+/**
+ * Implements drag, drop or move actions of {@link GraphicalNode} on map.
+ */
+@SuppressWarnings("serial")
+public class MapActions extends DropTarget {
+	private JComponent component;
+	private final String ACTION_MOVE = "MoveNode";
+	private final String ACTION_ADD = "AddNode";
+
+	public MapActions(JComponent comp) {
+		this.component = comp;
+	}
+
+	public void drop(DropTargetDropEvent dropEvent) {
+
+		if (dropEvent.isDataFlavorSupported(GraphicalNode.dataFlavor)) {
+			dropEvent.acceptDrop(dropEvent.getDropAction());
+
+			JLayeredPane panel = (JLayeredPane) this.component;
+			Transferable t = dropEvent.getTransferable();
+			
+			try {
+				String actionType = ACTION_ADD;
+				GraphicalNode node = (GraphicalNode) t.getTransferData(GraphicalNode.dataFlavor);
+				Dimension size = node.getPreferredSize();
+				
+				if (!node.isShouldRemoved()) {
+					panel.add(node);
+					FileLogger.write("ACTION=" + actionType + "_START", FileLogger.MSG_TYPE_REPLAY);
+				}else
+				{
+					actionType = ACTION_MOVE;
+					FileLogger.write("ACTION=" + actionType + "_START", FileLogger.MSG_TYPE_REPLAY);
+				}
+				
+				node.setSelectGNode();
+				node.setBounds(dropEvent.getLocation().x, dropEvent.getLocation().y, size.width, size.height);
+				node.fillNodePanel();
+				
+				panel.invalidate();
+				node.myForm.refreshPowerShower();
+
+				if (actionType.equals(ACTION_ADD)) {
+					FileLogger.write("NodeName=" + node.getName(), FileLogger.MSG_TYPE_REPLAY);
+					FileLogger.write("NodeIP=" + node.getNode().getIP(), FileLogger.MSG_TYPE_REPLAY);
+					FileLogger.write("NodeXCoord=" + node.getX(), FileLogger.MSG_TYPE_REPLAY);
+					FileLogger.write("NodeYCoord=" + node.getY(), FileLogger.MSG_TYPE_REPLAY);
+				}else{
+					FileLogger.write("NodeName=" + node.getName(), FileLogger.MSG_TYPE_REPLAY);
+					FileLogger.write("NodeXCoord=" + node.getX(), FileLogger.MSG_TYPE_REPLAY);
+					FileLogger.write("NodeYCoord=" + node.getY(), FileLogger.MSG_TYPE_REPLAY);
+				}
+				
+				dropEvent.dropComplete(true);
+				FileLogger.write("ACTION=" + actionType + "_DONE", FileLogger.MSG_TYPE_REPLAY);
+				
+			} catch (UnsupportedFlavorException ufe) {
+				FileLogger.write("Error dropping node, unsupported flavor: " + ufe, FileLogger.MSG_TYPE_ERROR);
+			} catch (IOException ioe) {
+				FileLogger.write("Error dropping node, IO exception: " + ioe, FileLogger.MSG_TYPE_ERROR);
+			}
+		}
+	}
+}
