f13bc18b95cc092d2d7df50efce31a4af06ffdab
quiksillvr <quiksillvr@a09ce2f9-38f1-0d4c-c202-f4d1de138eeb>
2010-12-04 20:34:19 +0000

    Added replay logs

--- gr1-manet-simulator/src/UI/actions/SaveNodeAction.java
+++ gr1-manet-simulator/src/UI/actions/SaveNodeAction.java
@@ -17,6 +17,8 @@ package UI.actions;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
+
+import logger.FileLogger;
 import UI.Myform;
 import UI.NodeProperties;
 import UI.myobjects.GraphicalNode;
@@ -29,22 +31,35 @@ public class SaveNodeAction implements ActionListener {
     }
 
     /**
-     * it should load information from right pane (Node Panel Properties) and set selected graphical node properties
+     * Saves/Updates node properties entered from the properties section.
      * @param e
      */
     public void actionPerformed(ActionEvent e) {
+    	
         if (myForm.getSelectedGNode()!=null){
-            GraphicalNode gNode = myForm.getSelectedGNode();
+            
+        	GraphicalNode gNode = myForm.getSelectedGNode();
             NodeProperties np = myForm.getNodePanel();
             gNode.setName(np.nameText.getText().trim().toLowerCase());
             gNode.setNodeIP(np.ipText.getText());
             gNode.setScaledCoordinates(Integer.parseInt(np.xCordText.getText()), Integer.parseInt(np.yCordText.getText()));
             gNode.setColor(np.colorBtn.getBackground());
             gNode.setNodePower(Integer.parseInt(np.powerText.getText()));
+            
             if (np.nameText.getText().trim().length()>0){
                 np.nameText.setEnabled(false);
             }
+            
             myForm.refreshPowerShower();
+            
+            FileLogger.write("ACTION=UpdateNodeProps_START", FileLogger.MSG_TYPE_REPLAY);
+            FileLogger.write("NodeName=" + gNode.getName(), FileLogger.MSG_TYPE_REPLAY);
+			FileLogger.write("NodeIP=" + gNode.getNode().getIP(), FileLogger.MSG_TYPE_REPLAY);
+			FileLogger.write("NodeXCoord=" + gNode.getX(), FileLogger.MSG_TYPE_REPLAY);
+			FileLogger.write("NodeYCoord=" + gNode.getY(), FileLogger.MSG_TYPE_REPLAY);
+			FileLogger.write("NodeColor=" + gNode.getColor().toString(), FileLogger.MSG_TYPE_REPLAY);
+			FileLogger.write("NodePower=" + gNode.getNode().getPower(), FileLogger.MSG_TYPE_REPLAY);
+            FileLogger.write("ACTION=UpdateNodeProps_DONE", FileLogger.MSG_TYPE_REPLAY);
 
         }else{
             JOptionPane.showMessageDialog(myForm,"Please Select a Node");
