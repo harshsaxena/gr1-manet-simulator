83913c67671cf7d3285e9725ccd0d55b0047d66c
jwoozeR <jwoozeR@a09ce2f9-38f1-0d4c-c202-f4d1de138eeb>
2010-12-06 03:29:11 +0000

    fixed integer overflow bug in Bellman-Ford

--- gr1-manet-simulator/src/UI/actions/SendMsgAction.java
+++ gr1-manet-simulator/src/UI/actions/SendMsgAction.java
@@ -44,40 +44,50 @@ public class SendMsgAction implements ActionListener {
 		FileLogger.write("ACTION=SendMsgs_START", FileLogger.MSG_TYPE_REPLAY);
 
 		// get From node
-		GraphicalNode fromGNode = myForm.getGNode(myForm.getNodePropertiesPanel().sendFromText.getText().trim().toLowerCase());
-		String procotolSelection = myForm.getNodePropertiesPanel().protocolComboBox.getSelectedItem().toString();
+		GraphicalNode fromGNode = myForm.getGNode(myForm
+				.getNodePropertiesPanel().sendFromText.getText().trim()
+				.toLowerCase());
+		String procotolSelection = myForm.getNodePropertiesPanel().protocolComboBox
+				.getSelectedItem().toString();
 		NodeProperties np = new NodeProperties();
-		if(procotolSelection.equals(np.getKEY_DSDV()))
-		{
+		if (procotolSelection.equals(np.getKEY_DSDV())) {
 			Map_Manager.get_instance().setMode(Protocol.DSDV);
-		}else{
+		} else {
 			Map_Manager.get_instance().setMode(Protocol.AODV);
 		}
-		
+
 		ReplayLogger.logSendNodeMsg(fromGNode, 0, fromGNode.getName());
 
 		// get To nodes
 		List<GraphicalNode> destList = new ArrayList<GraphicalNode>();
-		StringTokenizer tok = new StringTokenizer(myForm.getNodePropertiesPanel().sendToText.getText().trim().toLowerCase()," ,");
+		StringTokenizer tok = new StringTokenizer(myForm
+				.getNodePropertiesPanel().sendToText.getText().trim()
+				.toLowerCase(), " ,");
 		while (tok.hasMoreTokens()) {
 			GraphicalNode g = myForm.getGNode(tok.nextToken());
 			if (g != null)
 				ReplayLogger.logSendNodeMsg(g, 1, g.getName());
-				destList.add(g);
+			destList.add(g);
 		}
-		
-		FileLogger.write("\tMsg=" + myForm.getNodePropertiesPanel().msgText.getText(), FileLogger.MSG_TYPE_REPLAY);
-		FileLogger.write("\tProtocol=" + procotolSelection, FileLogger.MSG_TYPE_REPLAY);
+
+		FileLogger.write("\tMsg="
+				+ myForm.getNodePropertiesPanel().msgText.getText(),
+				FileLogger.MSG_TYPE_REPLAY);
+		FileLogger.write("\tProtocol=" + procotolSelection,
+				FileLogger.MSG_TYPE_REPLAY);
 
 		// send to each destination
 		for (GraphicalNode dest : destList) {
 			if (fromGNode != null && dest != null) {
-				new SendDataThread(fromGNode.getNode(), dest.getNode(), new Data(myForm.getNodePropertiesPanel().msgText.getText()));
+				new SendDataThread(fromGNode.getNode(), dest.getNode(),
+						new Data(myForm.getNodePropertiesPanel().msgText
+								.getText()));
 			} else {
-				JOptionPane.showMessageDialog(myForm, "Destination node not found!");
+				JOptionPane.showMessageDialog(myForm,
+						"Destination node not found!");
 			}
 		}
-		
+
 		FileLogger.write("ACTION=SendMsgs_END", FileLogger.MSG_TYPE_REPLAY);
 	}
 }
