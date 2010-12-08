e3298d9ceb0d7a313c2d50f86f084ad710f3d840
quiksillvr <quiksillvr@a09ce2f9-38f1-0d4c-c202-f4d1de138eeb>
2010-12-08 03:41:30 +0000

    Added some add node replay logic.

--- gr1-manet-simulator/src/replay/ReplayFileParser.java
+++ gr1-manet-simulator/src/replay/ReplayFileParser.java
@@ -14,29 +14,123 @@
 
 package replay;
 
+import java.awt.Dimension;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-
-import UI.myobjects.NodeButton;
+import javax.swing.JLayeredPane;
 
 import logger.FileLogger;
+import simulator.Map_Manager;
+import simulator.noderelated.Coordinates;
+import simulator.noderelated.IPAddress;
+import UI.Myform;
+import UI.myobjects.GraphicalNode;
 
 public class ReplayFileParser {
 	
-	public static void readLineByLine() throws FileNotFoundException{
+	private String nodeName = "";
+	private String ip = "";
+	private String nodeXCoord = "";
+	private String nodeYCoord = "";
+	private String power = "";
+	private String dropEventAction = "";
+	private String dropEventLocX = "";
+	private String dropEventLocY = "";
+	private String dropEventSourceAction = "";
+	private Myform myForm;
+	
+	
+	
+	public ReplayFileParser(Myform myform) {
+		this.myForm = myform;
+	}
+
+	public String getNodeName() {
+		return nodeName;
+	}
+
+	public void setNodeName(String nodeName) {
+		this.nodeName = nodeName;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public String getNodeXCoord() {
+		return nodeXCoord;
+	}
+
+	public void setNodeXCoord(String nodeXCoord) {
+		this.nodeXCoord = nodeXCoord;
+	}
+
+	public String getNodeYCoord() {
+		return nodeYCoord;
+	}
+
+	public void setNodeYCoord(String nodeYCoord) {
+		this.nodeYCoord = nodeYCoord;
+	}
+
+	public String getPower() {
+		return power;
+	}
+
+	public void setPower(String power) {
+		this.power = power;
+	}
+
+	public String getDropEventAction() {
+		return dropEventAction;
+	}
+
+	public void setDropEventAction(String dropEventAction) {
+		this.dropEventAction = dropEventAction;
+	}
+
+	public String getDropEventLocX() {
+		return dropEventLocX;
+	}
+
+	public void setDropEventLocX(String dropEventLocX) {
+		this.dropEventLocX = dropEventLocX;
+	}
+
+	public String getDropEventLocY() {
+		return dropEventLocY;
+	}
+
+	public void setDropEventLocY(String dropEventLocY) {
+		this.dropEventLocY = dropEventLocY;
+	}
+
+	public String getDropEventSourceAction() {
+		return dropEventSourceAction;
+	}
+
+	public void setDropEventSourceAction(String dropEventSourceAction) {
+		this.dropEventSourceAction = dropEventSourceAction;
+	}
+
+	public Myform getMyForm() {
+		return myForm;
+	}
+
+	public void setMyForm(Myform myForm) {
+		this.myForm = myForm;
+	}
+
+	public void readLineByLine() throws FileNotFoundException{
 		File logFile = new File(FileLogger.logFile);
 		Scanner scanner = new Scanner(new FileReader(logFile));
-		
-		String nodeName = "";
-		String ip = "";
-		String nodeXCoord = "";
-		String nodeYCoord = "";
-		String power;
 		
 		try {
 			while(scanner.hasNextLine()){
@@ -44,7 +138,7 @@ public class ReplayFileParser {
 				if(next.contains("ACTION=AddNode_START")){
 					while(scanner.hasNextLine()){
 						String next2 = scanner.nextLine();
-						if(!next2.contains("ACTION=AddNode_STOP")){
+						
 							
 							if(next2.contains("NodeName")){
 								Scanner scanAddLine = new Scanner(next2);
@@ -95,14 +189,53 @@ public class ReplayFileParser {
 									}
 								}
 							}
-						}else
-						{
-							processAddNode();
+							
+							if(next2.contains("DropEvent_DropAction")){
+								Scanner scanAddLine = new Scanner(next2);
+								while(scanAddLine.hasNext()){
+									scanAddLine.useDelimiter("=");
+									if (!scanAddLine.next().equals("DropEvent_DropAction")) {
+										dropEventAction = scanAddLine.next();
+									}
+								}
+							}
+							
+							if(next2.contains("DropEvent_LocX")){
+								Scanner scanAddLine = new Scanner(next2);
+								while(scanAddLine.hasNext()){
+									scanAddLine.useDelimiter("=");
+									if (!scanAddLine.next().equals("DropEvent_LocX")) {
+										dropEventLocX = scanAddLine.next();
+									}
+								}
+							}
+							
+							if(next2.contains("DropEvent_LocY")){
+								Scanner scanAddLine = new Scanner(next2);
+								while(scanAddLine.hasNext()){
+									scanAddLine.useDelimiter("=");
+									if (!scanAddLine.next().equals("DropEvent_LocY")) {
+										dropEventLocY = scanAddLine.next();
+									}
+								}
+							}
+							
+							if(next2.contains("DropEvent_SourceAction")){
+								Scanner scanAddLine = new Scanner(next2);
+								while(scanAddLine.hasNext()){
+									scanAddLine.useDelimiter("=");
+									if (!scanAddLine.next().equals("DropEvent_SourceAction")) {
+										dropEventSourceAction = scanAddLine.next();
+									}
+								}
+							}
+							
+							if(next2.equals("ACTION=AddNode_END")){
+								reAddNode();
+							}
 						}
 						
-						
 					}
-				}
 			}
 		} finally
 		{
@@ -110,9 +243,40 @@ public class ReplayFileParser {
 		}
 	}
 
-	private static void processAddNode() {
-		NodeButton nodeButton = new NodeButton(new ImageIcon("images/SendingNode0.png"));
+	private void reAddNode() {
 		
+		GraphicalNode reAddGNode = new GraphicalNode(myForm.addNodeBtn.getIcon(), myForm, false);
+		reAddGNode.setVisible(true);
+		reAddGNode.setName(nodeName);
+		IPAddress ipAddress = new IPAddress(ip);
+		reAddGNode.getNode().setIP(ipAddress);
+		reAddGNode.setLocation(Integer.parseInt(nodeXCoord), Integer.parseInt(nodeYCoord));
+		Coordinates coords = new Coordinates(Integer.parseInt(nodeXCoord), Integer.parseInt(nodeYCoord));
+		reAddGNode.getNode().setNode_coordinates(coords);
+		reAddGNode.getNode().setPower(Integer.parseInt(power));
+		
+		myForm.getGraphicalNodes().add(reAddGNode);
+		Map_Manager.get_instance().getNode_list().add(reAddGNode.getNode());
+		
+		JLayeredPane panel = (JLayeredPane) myForm.getMyMap();
+		
+		Dimension size = reAddGNode.getPreferredSize();
+		
+		if (!reAddGNode.isShouldRemoved()) {
+			try {
+				panel.add(reAddGNode);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		reAddGNode.setSelectGNode();
+		reAddGNode.setBounds(reAddGNode.getX(), reAddGNode.getY(), size.width, size.height);
+		reAddGNode.fillNodePanel();
+		
+		panel.invalidate();
+		reAddGNode.myForm.refreshPowerShower();
+
 	}
 
 }
