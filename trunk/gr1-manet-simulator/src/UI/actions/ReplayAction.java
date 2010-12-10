b3113559b804e110fd002b57ed5d65830fb2254e
jwoozeR <jwoozeR@a09ce2f9-38f1-0d4c-c202-f4d1de138eeb>
2010-12-10 21:28:01 +0000

    resolved conflict in ReplayAction

--- trunk/gr1-manet-simulator/src/UI/actions/ReplayAction.java
+++ trunk/gr1-manet-simulator/src/UI/actions/ReplayAction.java
@@ -23,12 +23,14 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 import replay.ReplayFileParser;
+import simulator.Map_Manager;
+import simulator.Node;
 import UI.Myform;
 import UI.myobjects.GraphicalNode;
 
 /**
  * @author mroberts
- *
+ * 
  */
 public class ReplayAction implements ActionListener {
 	private Myform myForm;
@@ -36,13 +38,13 @@ public class ReplayAction implements ActionListener {
 	public ReplayAction(Myform myForm) {
 		this.setMyForm(myForm);
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
-		//FileLogger.write("ACTION=Replay_START", FileLogger.MSG_TYPE_REPLAY);
-		
+		// FileLogger.write("ACTION=Replay_START", FileLogger.MSG_TYPE_REPLAY);
+
 		// Clear nodes for replay
 		clearNodesFromMap();
-		
+
 		// Parse log for node properties
 		try {
 			ReplayFileParser replayFileParser = new ReplayFileParser(myForm);
@@ -50,20 +52,29 @@ public class ReplayAction implements ActionListener {
 		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
 		}
-		
-		//FileLogger.write("ACTION=Replay_END", FileLogger.MSG_TYPE_REPLAY);
+
+		// FileLogger.write("ACTION=Replay_END", FileLogger.MSG_TYPE_REPLAY);
 	}
 
 	/**
 	 * Clears nodes from the map.
 	 */
-	private void clearNodesFromMap() {		
+	private void clearNodesFromMap() {
+		// List<GraphicalNode> gNodeList = myForm.getGraphicalNodes();
+		// for (int i = gNodeList.size() - 1; i >= 0; i--) {
+		// GraphicalNode gNode = gNodeList.get(i);
+		// myForm.getMyMap().remove(gNode);
+		// myForm.getGraphicalNodes().remove(gNode);
+		// }
+
 		List<GraphicalNode> gNodeList = myForm.getGraphicalNodes();
-		for(int i = gNodeList.size()-1; i >=0; i--){
-			GraphicalNode gNode = gNodeList.get(i);
-			myForm.getMyMap().remove(gNode);
-			myForm.getGraphicalNodes().remove(gNode);
+		List<Node> nodeList = Map_Manager.get_instance().getNode_list();
+		for (GraphicalNode node : gNodeList) {
+			myForm.getMyMap().remove(node);
 		}
+		gNodeList.removeAll(gNodeList);
+		nodeList.removeAll(nodeList);
+
 		myForm.getNodePropertiesPanel().clearNodeProperties();
 		myForm.setSelectedGNode(null);
 	}
@@ -75,5 +86,5 @@ public class ReplayAction implements ActionListener {
 	public Myform getMyForm() {
 		return myForm;
 	}
-	
+
 }
