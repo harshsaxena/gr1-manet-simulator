61b739f6a376d06981376e076b82dcd2b4f4f219
quiksillvr <quiksillvr@a09ce2f9-38f1-0d4c-c202-f4d1de138eeb>
2010-12-10 23:21:15 +0000

    Initial check in

---
+++ trunk/gr1-manet-simulator/src/UI/actions/HelpAction.java
@@ -0,0 +1,28 @@
+/**  
+ * ----------------------------------------------------------
+ * This software is for educational purposes only.
+ * The base of this software was created by IntelliJ IDEA.
+ * Additions to the base have been made by the Hood College
+ * Computer Science Department, Graduate Group 1.
+ * ----------------------------------------------------------
+ *
+ * History:
+ * @version: $Revision$
+ * @date: $Date$
+ * @author: $Author$
+ */
+
+package UI.actions;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HelpAction implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
