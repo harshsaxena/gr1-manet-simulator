b1711108e3c44c4f0b9a5b8203f54032a2317847
quiksillvr <quiksillvr@a09ce2f9-38f1-0d4c-c202-f4d1de138eeb>
2010-12-13 15:07:56 +0000

    Added action for opening the user's guide.

--- gr1-manet-simulator/src/UI/actions/HelpAction.java
+++ gr1-manet-simulator/src/UI/actions/HelpAction.java
@@ -1,4 +1,4 @@
-/**  
+/**
  * ----------------------------------------------------------
  * This software is for educational purposes only.
  * The base of this software was created by IntelliJ IDEA.
@@ -14,34 +14,42 @@
 
 package UI.actions;
 
+import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.IOException;
 
 import UI.Myform;
 
 public class HelpAction implements ActionListener {
-	public final static String docDirPath = "./doc/";
-	public final static String helpDoc = "testhelp.pdf";
+	public final static String helpDoc = "MANET_Simulator_UserGuide.pdf";
 	private Myform myForm;
-	
+
 	public HelpAction(Myform myForm) {
 		this.setMyForm(myForm);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		
-		File helpFile = new File(docDirPath + helpDoc);
-		
-	}
 
-	public void setMyForm(Myform myForm) {
-		this.myForm = myForm;
+		File helpFile = new File(helpDoc);
+		helpFile = helpFile.getAbsoluteFile();
+		// String helpPath = helpFile.getAbsolutePath();
+		Desktop desktop = Desktop.getDesktop();
+		try {
+			desktop.open(helpFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public Myform getMyForm() {
 		return myForm;
+	}
+
+	public void setMyForm(Myform myForm) {
+		this.myForm = myForm;
 	}
 
 }
